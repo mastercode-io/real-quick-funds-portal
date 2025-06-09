793b40eb2ca7e592d083331dbc3cb6ae0a90c99d
alexsherin <alex.sherin@gmail.com>
2025-06-09 19:54:16 +0100

    Netlify deployment config

--- remix.config.js
+++ remix.config.js
@@ -3,4 +3,7 @@ export default {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   postcss: true,
+  serverBuildTarget: "netlify",
+  publicPath: "/build/",
+  assetsBuildDirectory: "public/build",
 };
