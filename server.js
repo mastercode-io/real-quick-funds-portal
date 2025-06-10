9655f30203e1e8532b2fb07e823f2e5bd7850db1
alexsherin <alex.sherin@gmail.com>
2025-06-10 08:34:31 +0100

    netlify deployment fix 4

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const { createRequestHandler } = require("@remix-run/netlify");
-const build = require("@remix-run/dev/server-build");
 
+/**
+ * A function that defines the Netlify request handler.
+ * @type {import('@remix-run/netlify').RequestHandler}
+ */
 exports.handler = createRequestHandler({
-  build,
+  // When running in development, we'll use the build from the dev server.
+  // Otherwise, use the build that was generated in the build step.
+  build: process.env.NODE_ENV === "development" 
+    ? require("@remix-run/dev/server-build") 
+    : require("./build"),
   mode: process.env.NODE_ENV,
 });
